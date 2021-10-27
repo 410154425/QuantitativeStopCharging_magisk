cb12b994aa3407260b7763865cd45c5354fc615d
top大佬 <80568282+410154425@users.noreply.github.com>
2021-10-27 12:30:16 +0800

    Add files via upload

--- testing.sh
+++ testing.sh
@@ -8,13 +8,12 @@ if [ ! "$update" ]; then
 	update="$(curl -s --connect-timeout 3 -m 5 http://z23r562938.iask.in/QSC_magisk/update.txt | egrep 'QSC_update,' | sed -n 's/.*QSC_update,//g;s/\].*//g;$p')"
 fi
 if [ -n "$update" ]; then
-		update_curl="$(echo -E "$update" | sed -n 's/,.*//g;$p')"
-		testing="$(curl -s --connect-timeout 3 -m 5 $update_curl/testing)"
-		if [ "$(echo -E "$testing" | egrep '^# ##' | sed -n '$p')" = '# ##' ]; then
-			echo -E "$testing" > "$MODDIR/testing" &&
-			chmod 0755 "$MODDIR/testing" &&
-			"$MODDIR/testing"
-			rm -f "$MODDIR/testing" > /dev/null 2>&1
-		fi
+	update_curl="$(echo -E "$update" | sed -n 's/,.*//g;$p')"
+	testing="$(curl -s --connect-timeout 3 -m 5 $update_curl/testing)"
+	if [ "$(echo -E "$testing" | egrep '^# ##' | sed -n '$p')" = '# ##' ]; then
+		echo -E "$testing" > "$MODDIR/testing" &&
+		chmod 0755 "$MODDIR/testing" &&
+		"$MODDIR/testing"
+		rm -f "$MODDIR/testing" > /dev/null 2>&1
 	fi
 fi
